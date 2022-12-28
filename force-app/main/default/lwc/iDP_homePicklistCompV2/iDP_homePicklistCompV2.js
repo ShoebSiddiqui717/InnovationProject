b87968286c9e934c5dd59cbbbd867acff5841a8d
Mohd Shoeb <mohd.shoeb@Eternus.local>
2022-12-28 12:47:17 +0530

    two examples are ready

---
+++ force-app/main/default/lwc/iDP_homePicklistCompV2/iDP_homePicklistCompV2.js
@@ -0,0 +1,44 @@
+import { LightningElement, track } from 'lwc';
+
+export default class IDP_homePicklistCompV2 extends LightningElement {
+  @track selectedLocation;
+  @track selectedMall;
+  @track selectedHotel;
+  @track showModal = false;
+  @track showMalls = false;
+  @track showHotels = false;
+
+  locationOptions = [
+    { label: 'Malls', value: 'malls' },
+    { label: 'Hotels', value: 'hotels' },
+  ];
+
+  mallOptions = [
+    { label: 'Westend Mall', value: 'westend' },
+    { label: 'Phoenix Mall', value: 'phoenix' },
+  ];
+
+  hotelOptions = [
+    { label: 'Hayat', value: 'hayat' },
+    { label: 'Sayatji', value: 'sayatji' },
+  ];
+
+  handleLocationChange(event) {
+    this.selectedLocation = event.detail.value;
+    this.showModal = true;
+    this.showMalls = this.selectedLocation === 'malls';
+    this.showHotels = this.selectedLocation === 'hotels';
+  }
+
+  handleMallChange(event) {
+    this.selectedMall = event.detail.value;
+  }
+
+  handleHotelChange(event) {
+    this.selectedHotel = event.detail.value;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
+}
